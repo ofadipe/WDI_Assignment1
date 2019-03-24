d18163cadb036a8efc3d559e41f54613c18e8a05
ofadipe <2fadio51@solent.ac.uk>
2019-03-24 12:45:29 +0000

    Using Nuxt JS instead too many issues

--- autoura-assignment/src/views/accessiblepoi.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-app class="red">
-    
-
-    <v-content>
-     <h1>Does this work2</h1>
-    </v-content>
-  </v-app>
-</template>
--- autoura-assignment/src/views/restaurants.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <v-app class="red">
-    
-
-    <v-content>
-     <h1>Does this work1</h1>
-    </v-content>
-  </v-app>
-</template>
