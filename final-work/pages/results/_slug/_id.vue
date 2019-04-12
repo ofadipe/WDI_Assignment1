fe9b1c7ece1f232f9a8808094ba3524210cf3938
ofadipe <2fadio51@solent.ac.uk>
2019-04-12 23:03:29 +0100

    sorted out name allignment

--- final-work/pages/results/_slug/_id.vue
+++ final-work/pages/results/_slug/_id.vue
@@ -18,14 +18,14 @@
             </v-flex>
             <v-flex xs4>{{object.summary}}</v-flex>
             <!--- Needs to be moved to the right more -->
-            <v-flex xs8 class="" >
+            <v-flex xs8 text-lg-right>
               <a :href="object.website_url" target="_blank">Website: {{object.website_url}}</a>
             </v-flex>
           </v-layout>
 
               </v-img>
         </v-flex>
-        <v-flex xs6 mt-4>
+        <v-flex xs6 mt-4 >
           <h2>About {{object.name}}</h2>
           {{object.description}}
         </v-flex>
@@ -50,7 +50,7 @@
             allowfullscreen
           ></iframe>
           <!-- Move to the right more -->
-          <img v-else :src="object.picture.url" :alt="'Photograph of ' + object.name" width="400" height="300">
+          <img auto v-else :src="object.picture.url" :alt="'Photograph of ' + object.name" width="400" height="300">
         </v-flex>
         <v-flex xs12 align-center text-xs-center class="mt-4" v-if="object.important !== ''">
           <h2>Important information</h2>
