e0adde4d2fae3dded38cfef3faf4768889db748b
Oluwaseun Fadipe <2fadio51@solent.ac.uk>
2019-04-22 02:07:58 +0100

    added keywords to meta

--- final-work/nuxt.config.js
+++ final-work/nuxt.config.js
@@ -13,6 +13,7 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: pkg.description },
+      {hid: 'keywords', name: 'keywords', content: 'oluwaseun, fadipe, autoura, vuejs, nuxt, wdi, solent, university, pwa, development, joe, appleton' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
