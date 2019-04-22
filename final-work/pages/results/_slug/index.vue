20942260ddd06c3bbd4993d31fd8e7c43f3ce784
Oluwaseun Fadipe <2fadio51@solent.ac.uk>
2019-04-22 02:16:03 +0100

    minor changes

--- final-work/pages/results/_slug/index.vue
+++ final-work/pages/results/_slug/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
-        <h1>the category: {{$route.params.slug}}</h1>
+        <h1>The Category: {{$route.params.slug}}</h1>
     </div>
 </template>
 <script>
 export default {
-    
+
 }
 </script>
 <style>
